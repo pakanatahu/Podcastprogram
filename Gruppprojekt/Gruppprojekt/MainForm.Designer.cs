a06b2685b3c4a0eeab0ce224a0032f25e691eaa5
pakanatahu <david.lundholm@live.se>
2017-10-31 15:03:25 +0100

    snyggat till lite kategori kod

--- Gruppprojekt/Gruppprojekt/MainForm.Designer.cs
+++ Gruppprojekt/Gruppprojekt/MainForm.Designer.cs
@@ -333,7 +333,6 @@
             this.Controls.Add(this.button1);
             this.Name = "MainForm";
             this.Text = "Form1";
-            this.Load += new System.EventHandler(this.MainForm_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
